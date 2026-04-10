532830374038a19f7659c0865a93083b50bae518
Harman1957 <hrmnk2005@gmail.com>
2026-04-10 12:30:54 +0530

    Create DBConnection.java

---
+++ SmartParking/src/DBConnection.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    public static Connection getConnection() {
+        try {
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/parking_db",
+                "root",
+                "**abc123"//your password
+            );
+
+            return con;
+
+        } catch (Exception e) {
+            System.out.println("❌ Database Connection Failed!");
+            System.out.println(e.getMessage()); // 🔥 important
+            return null;
+        }
+    }
+}
